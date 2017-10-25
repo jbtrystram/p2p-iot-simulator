d099a6859c31fdc62131b029dced48e0a41e0e42
JB Trystram <jbtrystram@redhat.com>
2017-10-25 15:43:35 +0200

    customize plot for Poster

--- plot/2d_graph.py
+++ plot/2d_graph.py
@@ -16,23 +16,32 @@ neigh_file="graphs/neighbors"+args.coord_file.lstrip("graphs/graph")
 
 coord = np.genfromtxt(args.coord_file, delimiter=';')
 
+#more awesomeness
+plt.xkcd()
+
 plt.figure()
-plt.ylabel('X')
-plt.xlabel('Y')
-axes = plt.gca()
+
+#plt.ylabel('X')
+#plt.xlabel('Y')
+plt.axis('off')  #remove axises
+axes = plt.gca() # auto-set ticks
+
 axes.set_xlim([0,1000])
 axes.set_ylim([0,1000])
-plt.title('Nodes position')
+plt.title('Node positions & discovered links')
 
-plt.scatter(coord[:,1], coord[:,2], s=60)
-
-#plot neigbors relations ships
+#plot neigbors relationships
 
 for line in open(neigh_file):
 	neighbors = np.fromstring( (line.rstrip('\n')), sep=';', dtype=int)
 	for node in range(1, neighbors.size):
 		A = neighbors[0]
 		B = neighbors[node]
-		plt.plot([coord[A,1], coord[B,1]], [coord[A,2], coord[B,2]])
+		plt.plot([coord[A,1], coord[B,1]], [coord[A,2], coord[B,2]],  linewidth=0.5, zorder=-1)
 
-plt.savefig(args.coord_file+'.map.png')
+# Plot nodes
+plt.scatter(coord[:,1], coord[:,2], s=7, zorder=1)
+
+plt.tight_layout()
+plt.savefig(args.coord_file+'.pdf')
+#plt.savefig(args.coord_file+'.map.png', dpi = (200))
