5ec5cfd4be6136a6bef136dc9f08efd0a12b719d
JB Trystram <jbtrystram@redhat.com>
2018-01-15 16:50:53 +0100

    added wrapper to easily parse CSV files

---
+++ src/example/update/EasyCSV.java
@@ -0,0 +1,49 @@
+package example.update;
+
+import com.univocity.parsers.csv.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class EasyCSV {
+
+    public List<String[]> content;
+
+    EasyCSV(String pathToFile){
+
+
+        //------------- FIELDS
+        File file = new File(pathToFile);
+
+
+        // CSV parser settings (newline character, separator..)
+        CsvParserSettings settings = new CsvParserSettings();
+        settings.getFormat().setLineSeparator("\n");
+
+        // creates a CSV parser
+        CsvParser parser = new CsvParser(settings);
+
+        // open the file to read
+        FileInputStream fis = null;
+
+        try {
+            fis = new FileInputStream(file);
+
+            // parses all rows in one go.
+            List<String[]> content = parser.parseAll(fis);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fis != null)
+                    fis.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
+
