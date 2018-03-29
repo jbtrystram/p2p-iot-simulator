f13ef8f87b2aa898aae5aab366a099c63885013b
JB Trystram <jbtrystram@redhat.com>
2018-03-29 16:58:06 +0200

    wassup

--- src/main/java/example/update/NetworkAgent.java
+++ src/main/java/example/update/NetworkAgent.java
@@ -1,12 +1,13 @@
 package example.update;
 
-import peersim.cdsim.CDProtocol;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import peersim.config.Configuration;
-import peersim.core.CommonState;
 import peersim.core.Node;
 import peersim.edsim.EDProtocol;
 
+import java.lang.reflect.Array;
 import java.util.*;
+
 
 public class NetworkAgent implements EDProtocol{
 
@@ -14,12 +15,13 @@ public class NetworkAgent implements EDProtocol{
     // Parameters
     // ------------------------------------------------------------------------
 
-    // Protocol to interact with : softwareDB and Downloader
+    // Protocol to interact with : softwareDB, neighborhood maintainer and Supervisor
     private static final String DB_PROT = "database_protocol";
     private static final String GOSSIP_PROT = "gossip_protocol";
+    private static final String NEIGH_PROT = "neighborhood_protocol";
 
     private static final String BANDW = "bandwidth";
-    private static final String DISK_SPACE = "disk_space";
+    private static final String PIECE_SIZE = "piece_size";
 
 
     // ------------------------------------------------------------------------
@@ -29,6 +31,16 @@ public class NetworkAgent implements EDProtocol{
 
     private final int dbPID;
     private final int gossipPID;
+    private final int neighborhoodPID;
+
+    private int bandwidth;
+    private final int pieceSize;
+
+    private boolean downloading = false;
+
+
+    // my local list <SoftwarePackage.ID, array>
+    ArrayList<Map.Entry<String, boolean[]>> localData;
 
 
     // constructor
@@ -39,6 +51,12 @@ public class NetworkAgent implements EDProtocol{
         // get PIDs of SoftwareDB and Downloader
         dbPID = Configuration.getPid(prefix + "." + DB_PROT);
         gossipPID = Configuration.getPid(prefix + "." + GOSSIP_PROT);
+        neighborhoodPID = Configuration.getPid(prefix + "." + NEIGH_PROT);
+
+        bandwidth = Configuration.getInt(prefix + "." +BANDW);
+        pieceSize = Configuration.getInt(prefix + "." +PIECE_SIZE);
+
+        localData = new ArrayList<>();
     }
 
 
@@ -46,12 +64,59 @@ public class NetworkAgent implements EDProtocol{
     public NetworkAgent clone(){
         return new NetworkAgent(prefix);
     }
-    // method trigerred by scheduler
 
 
+    // method trigerred by scheduler -> gives an ordered list of softwares to DL.
+    //TODO : do something cleaner & smarter in softwareDB instead of this.
+    public void update(List<SoftwareJob> packages) {
+        // update the local list.
+        for ( int i=0; i<packages.size() ;i++ ){
+            for (int j=0; j<localData.size() ;j++) {
 
-    //exchanging data with other peers
+                if (packages.get(i).getId().equals(localData.get(j).getKey())) {
+                    localData.add(i, localData.remove(j));
+                } else {
+                    localData.add(i, new AbstractMap.SimpleEntry(packages.get(i).getId(),
+                            new boolean[packages.get(i).size/pieceSize]));
+                }
+            }
+        }
+
+        //send request to neighbors
+
+    }
+    //receiving data from other peers
     public void processEvent(Node node, int i, Object o) {
 
+        // REQUEST
+
+        // RESPONSE
+
+        // PROCESS REQUEST
+
+    }
+
+    // send REQUEST messsages
+    private void requestNeighbors(){
+
+
+    }
+}
+
+class DataMessage{
+
+    final static int REQUEST = 0;
+    final static int RESPONSE = 1;
+
+    int type;
+
+    String hash;
+    int pieceNumber;
+
+
+    public DataMessage(int type, String hash, int pieceNumber) {
+        this.type = type;
+        this.hash = hash;
+        this.pieceNumber = pieceNumber;
     }
 }
