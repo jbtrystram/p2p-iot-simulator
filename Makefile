bec37586fe91379576981626856a95d49737e3a8
JB Trystram <jbtrystram@redhat.com>
2018-04-11 16:15:33 +0200

    WIP : naive software Agent 👷

--- Makefile
+++ Makefile
@@ -1,76 +1,13 @@
 VER=1.0.5
 
-.PHONY: all clean doc release
-
 all:
-	javac -classpath src:jep-2.3.0.jar:djep-1.0.0.jar:univocity-parsers-2.5.9.jar:maven-artifact-3.5.3.jar `find src -name "*.java"`
+	javac -classpath src:jep-2.3.0.jar:djep-1.0.0.jar:univocity-parsers-2.5.9.jar:maven-artifact-3.5.3.jar `find src/main/ -name "*.java"`
 clean:
-	rm -f `find src -name "*.class"`
-doc:
-	rm -rf doc/*
-	javadoc -overview overview.html -classpath src:jep-2.3.0.jar:djep-1.0.0.jar -d doc \
-                -group "Peersim" "peersim*" \
-                -group "Examples" "example.*" \
-		peersim \
-		peersim.cdsim \
-		peersim.config \
-		peersim.core \
-		peersim.dynamics \
-		peersim.edsim \
-		peersim.graph \
-		peersim.rangesim \
-		peersim.reports \
-		peersim.transport \
-		peersim.util \
-		peersim.vector \
-		example.aggregation \
-		example.loadbalance \
-		example.edaggregation \
-		example.hot \
-		example.newscast 
+	rm -f `find src/main -name "*.class"`
+run:
+	java -cp jep-2.3.0.jar:djep-1.0.0.jar:univocity-parsers-2.5.9.jar:maven-artifact-3.5.3.jar:src peersim.Simulator $(CONFIG).txt
 
-docnew:
-	rm -rf doc/*
-	javadoc -overview overview.html -docletpath peersim-doclet.jar -doclet peersim.tools.doclets.standard.Standard -classpath src:jep-2.3.0.jar:djep-1.0.0.jar -d doc \
-                -group "Peersim" "peersim*" \
-                -group "Examples" "example.*" \
-		peersim \
-		peersim.cdsim \
-		peersim.config \
-		peersim.core \
-		peersim.dynamics \
-		peersim.edsim \
-		peersim.graph \
-		peersim.rangesim \
-		peersim.reports \
-		peersim.transport \
-		peersim.util \
-		peersim.vector \
-		example.aggregation \
-		example.loadbalance \
-		example.hot \
-		example.edaggregation \
-		example.newscast 
-
-
-release: clean all docnew
-	rm -fr peersim-$(VER)
-	mkdir peersim-$(VER)
-	cp -r doc peersim-$(VER)
-	cp Makefile overview.html README CHANGELOG RELEASE-NOTES build.xml peersim-doclet.jar peersim-$(VER)
-	mkdir peersim-$(VER)/example
-	cp example/*.txt peersim-$(VER)/example
-	mkdir peersim-$(VER)/src
-	cp --parents `find src/peersim src/example -name "*.java"` peersim-$(VER)
-	cd src ; jar cf ../peersim-$(VER).jar `find peersim example -name "*.class"`
-	mv peersim-$(VER).jar peersim-$(VER)
-	cp jep-2.3.0.jar peersim-$(VER)
-	cp djep-1.0.0.jar peersim-$(VER)
-
-run:	
-	java -cp jep-2.3.0.jar:djep-1.0.0.jar:univocity-parsers-2.5.9.jar:maven-artifact-3.5.3.jar:src peersim.Simulator test.txt
-
-order: 
+order:
 	rm -rf graphs/
 	mkdir -p graphs
 	mv *.dat graphs/
--- src/main/java/example/update/NetworkAgent.java
+++ src/main/java/example/update/NetworkAgent.java
@@ -1,15 +1,19 @@
 package example.update;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
+import peersim.cdsim.CDProtocol;
 import peersim.config.Configuration;
+import peersim.core.CommonState;
 import peersim.core.Node;
 import peersim.edsim.EDProtocol;
+import peersim.edsim.EDSimulator;
 
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
-public class NetworkAgent implements EDProtocol{
+public class NetworkAgent implements EDProtocol, CDProtocol{
 
     // ------------------------------------------------------------------------
     // Parameters
@@ -67,7 +71,8 @@ public class NetworkAgent implements EDProtocol{
 
 
     // method trigerred by scheduler -> gives an ordered list of softwares to DL.
-    //TODO : do something cleaner & smarter in softwareDB instead of this.
+    //TODO : do something cleaner in a softwareDB class
+
     public void update(List<SoftwareJob> packages) {
         // update the local list.
         for ( int i=0; i<packages.size() ;i++ ){
@@ -76,30 +81,125 @@ public class NetworkAgent implements EDProtocol{
                 if (packages.get(i).getId().equals(localData.get(j).getKey())) {
                     localData.add(i, localData.remove(j));
                 } else {
-                    localData.add(i, new AbstractMap.SimpleEntry(packages.get(i).getId(),
+                    localData.add(i, new SimpleEntry(packages.get(i).getId(),
                             new boolean[packages.get(i).size/pieceSize]));
                 }
             }
         }
-
-        //send request to neighbors
-
     }
+
+    private Map.Entry<String, Integer> getRandomDownload() {
+
+       for (int i = 1; i <= localData.size(); i++) {
+
+           int rand = CommonState.r.nextInt(localData.get(i).getValue().length);
+
+           for (int j = rand; j < localData.get(i).getValue().length; j++) {
+               if (localData.get(i).getValue()[j] == false) {
+                   return new SimpleEntry<>(localData.get(i).getKey(), j);
+               }
+           }
+           for (int j = 0; j < rand; j++) {
+               if (localData.get(i).getValue()[j] == false) {
+                   return new SimpleEntry<>(localData.get(i).getKey(), j);
+               }
+           }
+       }
+       return null;
+    }
+
+
+
+    public void nextCycle(Node node, int pid) {
+        if (!downloading){
+
+            Map.Entry<String, Integer> toDownload = getRandomDownload();
+            if ( toDownload != null) {
+
+                //craft a new message
+                DataMessage msg = new DataMessage(DataMessage.REQUEST, toDownload.getKey(), toDownload.getValue(), node);
+                requestNeighbors(node, msg, pid);
+            }
+        }
+    }
+
     //receiving data from other peers
-    public void processEvent(Node node, int i, Object o) {
+    public void processEvent(Node localNode, int pid, Object data) {
 
-        // REQUEST
+        DataMessage event = (DataMessage)(data);
 
-        // RESPONSE
+        switch (event.type) {
 
-        // PROCESS REQUEST
+            case DataMessage.REQUEST:
+                if ( !downloading && event.sender != localNode && localDataContains(event.hash) ){
+                    // is this piece complete
+                    if ( localData.get(getLocalIndex(event.hash)).getValue()[event.pieceNumber] ) {
+                        DataMessage reply = new DataMessage(DataMessage.RESPONSE, event.hash, event.pieceNumber, localNode);
+                        EDSimulator.add(0, reply, event.sender, pid);
 
+                        //send the data
+                        this.downloading = true;
+                        // use the lowest bandwith of the 2 nodes
+                        int bdw = ( bandwidth < ((NetworkAgent)event.sender.getProtocol(pid)).bandwidth ) ?
+                                    bandwidth : ((NetworkAgent)event.sender.getProtocol(pid)).bandwidth ;
+                        reply = new DataMessage(DataMessage.DATA, event.hash, event.pieceNumber, localNode);
+                        EDSimulator.add(pieceSize/bdw, reply, event.sender, pid);
+
+                    }
+                }
+                break;
+
+            case DataMessage.RESPONSE:
+                //downloading data
+                this.downloading = true;
+                break;
+
+            case DataMessage.DATA:
+                //mark piece as completed into DB
+                localData.get(getLocalIndex(event.hash)).getValue()[event.pieceNumber] = true;
+
+                this.downloading = false;
+
+                //send ack
+                DataMessage msg = new DataMessage(DataMessage.DATAACK, event.hash, event.pieceNumber, localNode);
+                EDSimulator.add(0, msg, event.sender, pid);
+                break;
+
+                //TODO : what if energy cuts during a trasnfer? seeder is locked.
+            case DataMessage.DATAACK:
+                //we can stop uploading
+                this.downloading = false;
+                break;
+        }
+    }
+
+    private boolean localDataContains(String hash){
+        for (Map.Entry<String, boolean[]> tuple : localData) {
+            if (hash.equals(tuple.getKey())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int getLocalIndex(String hash){
+        for (int i=0; i<localData.size() ; i++) {
+            if (hash.equals(localData.get(i).getKey())) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     // send REQUEST messsages
-    private void requestNeighbors(){
+    private void requestNeighbors(Node localNode, DataMessage msg, int pid){
 
-
+        //get neighbors list
+        ((NeighborhoodMaintainer) localNode.getProtocol(neighborhoodPID)).getNeighbors()
+                .forEach(neighbor -> {
+                    // send request message to neighbor with no latency
+                    EDSimulator.add(0, msg, neighbor, pid);
+                });
     }
 }
 
@@ -107,14 +207,19 @@ class DataMessage{
 
     final static int REQUEST = 0;
     final static int RESPONSE = 1;
+    final static int DATA = 2;
+    final static int DATAACK = 3;
+     // ? //enum type1 {REQUEST, RESPONSE}
 
     int type;
 
     String hash;
     int pieceNumber;
+    Node sender;
 
 
-    public DataMessage(int type, String hash, int pieceNumber) {
+    public DataMessage(int type, String hash, int pieceNumber, Node sender) {
+        this.sender = sender;
         this.type = type;
         this.hash = hash;
         this.pieceNumber = pieceNumber;
