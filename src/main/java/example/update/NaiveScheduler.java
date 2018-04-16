a21437ceeef428c170fd449f3981291ee54bcbcf
JB Trystram <jbtrystram@redhat.com>
2018-04-16 11:15:22 +0200

    naive Scheduler

---
+++ src/main/java/example/update/NaiveScheduler.java
@@ -0,0 +1,18 @@
+package example.update;
+
+import java.util.*;
+
+/**
+ * Created by jibou on 16/04/18.
+ */
+public class NaiveScheduler extends Scheduler {
+
+    public NaiveScheduler(String prefix) {
+        super(prefix);
+    }
+
+    public void updateTasks(){
+
+    }
+
+}
