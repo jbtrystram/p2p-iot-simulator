8b5f23cf584fd64b4e9b2e522c6c2b1151edc6b4
Trystram Jean-Baptiste <jbtrystram@redhat.com>
2018-05-16 15:53:10 +0200

    initialise range with a normal distribution probability

---
+++ src/main/java/example/update/initialisation/RangeInitializer.java
@@ -0,0 +1,75 @@
+package example.update.initialisation;
+
+import example.update.constraints.NetworkRange;
+import peersim.config.Configuration;
+import peersim.core.CommonState;
+import peersim.core.Control;
+import peersim.core.Network;
+import peersim.core.Node;
+
+import java.io.ObjectInputFilter;
+
+public class RangeInitializer implements Control {
+
+    // ------------------------------------------------------------------------
+    // Parameters
+    // ------------------------------------------------------------------------
+    /**
+     * The protocol to operate on.
+     *
+     * @config
+     */
+    private static final String PAR_PROT = "protocol";
+    private static final String PAR_MEAN = "gaussian_mean";
+    private static final String PAR_DEV = "gaussian_deviation";
+
+
+    // ------------------------------------------------------------------------
+    // Fields
+    // ------------------------------------------------------------------------
+
+    /** Protocol identifier, obtained from config property {@link #PAR_PROT}. */
+    private final int pid;
+    private final int mean;
+    private final int deviation;
+
+    // ------------------------------------------------------------------------
+    // Constructor
+    // ------------------------------------------------------------------------
+
+    /**
+     * Standard constructor that reads the configuration parameters. Invoked by
+     * the simulation engine.
+     *
+     * @param prefix
+     *            the configuration prefix for this class.
+     */
+    public RangeInitializer(String prefix) {
+
+        pid = Configuration.getPid(prefix + "." + PAR_PROT);
+        mean = Configuration.getInt(prefix + "." + PAR_MEAN, 300);
+        deviation = Configuration.getInt(prefix + "." + PAR_DEV, 100);
+
+    }
+
+    // ------------------------------------------------------------------------
+    // Methods
+    // ------------------------------------------------------------------------
+    /**
+     * Initialize the range for each node following a normal distribution
+     * with the given settings.
+     */
+    public boolean execute() {
+
+        Node n ;
+        NetworkRange protocol;
+
+        for (int i = 0; i < Network.size(); i++) {
+            n = Network.get(i);
+            protocol = (NetworkRange) n.getProtocol(pid);
+            protocol.setRange((int)(CommonState.r.nextGaussian()*deviation+mean));
+        }
+        return false;
+    }
+
+}
