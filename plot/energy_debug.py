00feff2b468fda4048e6243a05ff5f1ae9fc97f5
JB Trystram <jbtrystram@redhat.com>
2018-05-17 09:58:13 +0200

    fix gitignore & indent

--- plot/energy_debug.py
+++ plot/energy_debug.py
@@ -26,11 +26,12 @@ def plotter(file_seq, path):
 
         #plot neigbors relationships
         for line in open(neigh_file):
-        	neighbors = np.fromstring( (line.rstrip('\n')), sep=';', dtype=int)
-        	for node in range(1, neighbors.size):
-        		A = neighbors[0]
-        		B = neighbors[node]
-        		plt.plot([coord[A,1], coord[B,1]], [coord[A,2], coord[B,2]],  linewidth=0.2, zorder=-1, c='0.5')
+                neighbors = np.fromstring( (line.rstrip('\n')), sep=';', dtype=int)
+                for node in range(1, neighbors.size):
+                        A = neighbors[0]
+                        B = neighbors[node]
+                        print("yolo")
+                        plt.plot([coord[A,1], coord[B,1]], [coord[A,2], coord[B,2]],  linewidth=0.2, zorder=-1, c='0.5')
 
         # Plot nodes
         plt.scatter(coord[:,1], coord[:,2], s=9, zorder=1)
