6ba42f2bf2e8608da26f2ed4dfb22e30c9db9735
Trystram Jean-Baptiste <jbtrystram@redhat.com>
2018-06-13 16:23:05 +0200

    2d graph now uses pandas

--- plot/2d_graph.py
+++ plot/2d_graph.py
@@ -1,38 +1,44 @@
-import numpy as np
+import pandas
 import matplotlib.pyplot as plt
 import argparse
 import os
+import math
 
 def plotter(file_seq, path):
     
-        coord = np.genfromtxt(path+"/graph_dump"+file_seq+".dat", delimiter=';')
-        neigh_file = path+"/neighbors_dump"+file_seq+".dat"
-        print("using "+"/data_pieces_dump"+file_seq+".dat")
-        progress = np.genfromtxt(path+"/progress_dump"+file_seq+".dat", delimiter=';', skip_header=1)
-        names = np.genfromtxt(path+"/progress_dump"+file_seq+".dat", delimiter=';', names=True)
+        coord = pandas.read_csv(path+"/graph_dump"+file_seq+".dat", header=None, delimiter=';')
+        neighbors = path+"/neighbors_dump"+file_seq+".dat"
+        print("Processing "+"/progress_dump"+file_seq+".dat")
+        progress = pandas.read_csv(path+"/progress_dump"+file_seq+".dat", delimiter=';')
 
-        plt.figure()
-        for item in range(1, progress.shape[1]-1):
+        #define subplot graph size
+        hauteur = math.floor(math.sqrt(len(progress.columns)))
+        largeur = math.ceil(math.sqrt(len(progress.columns)))
 
-                plt.subplot(1, progress.shape[1]-2, item, aspect='equal')
-                plt.title(names.dtype.names[item])
-                plt.axis('square')
+        key_index=1
+        for haut in  range(0,hauteur):
+                for large in range(1,largeur+1):
+                        if (key_index < len(progress.columns)-1):
+                                plt.subplot(hauteur, largeur, (largeur*haut)+(large), aspect='equal')
+                                plt.title(progress.columns[key_index])
+                                plt.axis('square')
 
-                #plot neigbors relationships
-                for line in open(neigh_file):
-                        neighbors = np.fromstring( (line.rstrip('\n')), sep=';', dtype=int)
-                        for node in range(1, neighbors.size):
-                                A = neighbors[0]
-                                B = neighbors[node]
-                                plt.plot([coord[A,1], coord[B,1]], [coord[A,2], coord[B,2]],  linewidth=0.2, zorder=-1, c='0.5')
+                                #plot neigbors relationships
+                                for line in open(neighbors):
+                                        neighbors_list = line.rstrip(';\n').split(';')
+                                        for node in range(1, len(neighbors_list)):
+                                                A = int(neighbors_list[0])
+                                                B = int(neighbors_list[node])
+                                                plt.plot([coord.iloc[A,1], coord.iloc[B,1]], [coord.iloc[A,2], coord.iloc[B,2]],  linewidth=0.2, zorder=-1, c='0.5')
 
-                # Plot nodes
-                plt.scatter(coord[:,1], coord[:,2], s=9, zorder=1, c=progress[:,item], vmin=0, vmax=100)
-                plt.xticks([], [])
-                plt.yticks([], [])
-                plt.xlim(0,1000)
-                plt.ylim(0,1000)
+                                # Plot nodes
+                                plt.scatter(coord.iloc[:,1], coord.iloc[:,2], s=9, zorder=1, c=progress.iloc[:,key_index], vmin=0, vmax=100)
+                                plt.xticks([], [])
+                                plt.yticks([], [])
+                                plt.xlim(0,1000)
+                                plt.ylim(0,1000)
 
+                                key_index+=1
         #plt.subplots_adjust() 
         #plt.tight_layout()
         #plt.savefig("figs/"+seq+'.pdf')
