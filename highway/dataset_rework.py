6d1122ddd79887609b0a838eec69e52dfc85edaf
Trystram Jean-Baptiste <jbtrystram@redhat.com>
2018-07-09 16:14:51 +0200

    remove unused colunms, use frameTime as time reference, offeset values to 0

--- highway/dataset_rework.py
+++ highway/dataset_rework.py
@@ -11,13 +11,10 @@ def percentage(total, progress):
         print(str(percent)+"%")
                 
 #needed columns
-cols_to_get = ['Vehicle_ID', 'Total_Frames', 'Global_Time', 'Global_X', 'Global_Y', 'v_Vel']
-data_in = "highway_101.csv"
+cols_to_get = ['Vehicle_ID', 'Total_Frames', 'Global_Time', 'Global_X', 'Global_Y']
+data_in = "highway/highway_101.csv"
 print("importing file : "+data_in)
 df = pandas.read_csv(data_in, usecols=cols_to_get, na_filter=False)
-
-#max_vID = df['Vehicle_ID'].max()
-#max_vID +=1
 
 print("Processing duplicates")
 uniques = df.drop_duplicates(subset=['Vehicle_ID', 'Total_Frames'])
@@ -27,6 +24,23 @@ for i in range(0, uniques.shape[0]):
         df.loc[(df['Vehicle_ID'] == uniques.iloc[i,0]) & (df['Total_Frames'] == uniques.iloc[i,1]), ['Vehicle_ID']] = i
         percentage(uniques.shape[0] ,i)
 
+print("Offsetting timestamps")
+minitime = df['Global_Time'].min()
+df['Global_Time'] = df['Global_Time'] - minitime
+
+minix = df['Global_X'].min()
+miniy = df['Global_Y'].min()
+df['Global_X'] = df['Global_X'] - minix
+df['Global_Y'] = df['Global_Y'] - miniy
+
+print("Sort by ascending time")
+df.sort_values('Global_Time', inplace=True)
+
+print("Casting coordinates into integers")
+df['global_X'] = df['Global_X'].astype(int)
+df['global_Y'] = df['Global_Y'].astype(int)
+
 data_out = "highway_101_fixed.csv"
+cols_to_out = ['Global_Time', 'Vehicle_ID', 'Global_X', 'Global_Y']
 print("Saving new dataset : "+data_out)
-df.to_csv(data_out, sep=';')
+df[cols_to_out].to_csv(data_out, sep=';', index=False)
