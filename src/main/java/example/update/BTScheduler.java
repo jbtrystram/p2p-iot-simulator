7cd096c8f3c49db6e95ef9e16722667be3036e16
Trystram Jean-Baptiste <jbtrystram@redhat.com>
2018-10-08 17:36:02 +0200

    use scheduler as a base class, draft bt scheduler
    Signed-off-by: Trystram Jean-Baptiste <jbtrystram@redhat.com>

--- src/main/java/example/update/BTScheduler.java
+++ src/main/java/example/update/BTScheduler.java
@@ -3,10 +3,9 @@ package example.update;
 import peersim.config.Configuration;
 import peersim.core.Node;
 
-import java.util.*;
+import java.util.ArrayList;
 
-
-public class NaiveScheduler extends Scheduler {
+public class BTScheduler extends Scheduler {
 
     @Override
     public Scheduler clone() {
@@ -14,7 +13,7 @@ public class NaiveScheduler extends Scheduler {
     }
 
     String prefix;
-    public NaiveScheduler(String prefix) {
+    public BTScheduler(String prefix) {
         this.prefix = prefix;
 
         // get PIDs of gossiper and Downloader
@@ -24,17 +23,14 @@ public class NaiveScheduler extends Scheduler {
         jobsList = new ArrayList<>();
         this.spaceProtocol = Configuration.getPid(prefix + "." + DISK_SPACE_PROT);
         this.bandwidthProtocol = Configuration.getPid(prefix + "." + BANDW_PROT);
-        }
+    }
 
 
     private int cycle_counter=0;
+    // This method is called each cycle and this where the strategy is implemented.
+    // the job list must be updated according to the proper strategy.
+    //TODO : some feedback from net agent
     public void updateTasks(Node localNode){
-        if (cycle_counter % 4 == 0) {
-            // TODO : gossip completed jobs ?
-            // periodically, gossip all the packages once
-            gossipAllJobs(localNode);
-        }
-        cycle_counter +=1;
-    }
 
+    }
 }
--- src/main/java/example/update/Scheduler.java
+++ src/main/java/example/update/Scheduler.java
@@ -3,24 +3,23 @@ package example.update;
 import example.update.constraints.Bandwidth;
 import example.update.strategies.Storage;
 import peersim.cdsim.CDProtocol;
-import peersim.config.Configuration;
 import peersim.core.Node;
 import peersim.edsim.EDProtocol;
 
 import java.util.*;
 
-public class Scheduler implements EDProtocol, CDProtocol{
+public abstract class Scheduler implements EDProtocol, CDProtocol{
 
     // ------------------------------------------------------------------------
     // Parameters
     // ------------------------------------------------------------------------
 
     // Protocol to interact with : softwareDB and Downloader
-    private static final String NET_PROT = "networking_protocol";
-    private static final String GOSSIP_PROT = "gossip_protocol";
+    static final String NET_PROT = "networking_protocol";
+    static final String GOSSIP_PROT = "gossip_protocol";
 
-    private static final String BANDW_PROT = "bandwidth_protocol";
-    private static final String DISK_SPACE_PROT = "disk_space_protocol";
+    static final String BANDW_PROT = "bandwidth_protocol";
+    static final String DISK_SPACE_PROT = "disk_space_protocol";
 
 
     // ------------------------------------------------------------------------
@@ -31,109 +30,51 @@ public class Scheduler implements EDProtocol, CDProtocol{
      * protocol identifiers. Obtained from config property
      * {@link #NET_PROT}, {@link #GOSSIP_PROT}.
      */
-    private final int netPID;
-    private final int gossipPID;
-    private int cycle_counter =0;
+    int netPID;
+    int gossipPID;
 
-    private ArrayList<SoftwareJob> jobsList;
+    ArrayList<SoftwareJob> jobsList;
     int spaceProtocol;
     int bandwidthProtocol;
-
-    String prefix;
-    public Scheduler(String prefix) {
-        this.prefix = prefix;
-
-        // get PIDs of gossiper and Downloader
-        gossipPID = Configuration.getPid(prefix + "." + GOSSIP_PROT);
-        netPID = Configuration.getPid(prefix + "." + NET_PROT);
-
-        jobsList = new ArrayList<>();
-        this.spaceProtocol = Configuration.getPid(prefix + "." + DISK_SPACE_PROT);
-        this.bandwidthProtocol = Configuration.getPid(prefix + "." + BANDW_PROT);
-    }
-
-    public Scheduler clone(){
-        return new Scheduler(prefix);
-    }
 
     //receive messages
     public void processEvent(Node node, int pid, Object event) {
         // not used yet
     }
 
-    //Compute a job cost
-    private void priceIT(SoftwareJob job){
-        job.setCost(job.size*job.expectedQoS);
-    }
-
-    //process event received from gossiper
+       //process event received from gossiper
     public void processGossipMessage(Node localNode, SoftwareJob newJob){
 
         if( ! newJob.isExpired() && newJob.isDoable( ((Bandwidth) localNode.getProtocol(bandwidthProtocol)).getDownlinkCapacity() )
                 && ((Storage)localNode.getProtocol(spaceProtocol)).allocateStorage(newJob.size) ) {
-            priceIT(newJob);
+
             jobsList.add(newJob);
-            updateTasks();
+            updateTasks(localNode);
         }
     }
 
-
     public void nextCycle(Node node, int protocolID) {
 
-        updateTasks();
+        updateTasks(node);
 
         //pass list to the downlader
         if (! jobsList.isEmpty()) {
             ((NetworkAgent) node.getProtocol(netPID)).update(jobsList);
-
-            if (cycle_counter % 4 == 0) {
-                // TODO : gossip completed jobs ?
-                // periodically, gossip all the packages once
-                Gossiper localGossiper = (Gossiper) node.getProtocol(gossipPID);
-
-                jobsList.forEach(soft -> {
-                    localGossiper.gossip(node, node, gossipPID, soft);
-                });
-            }
         }
-        cycle_counter += 1;
     }
 
+    void gossipAllJobs(Node localNode){
+        Gossiper localGossiper = (Gossiper) localNode.getProtocol(gossipPID);
 
-    // TODO : feedback from DL Agent to take into account network stats
-    public void updateTasks(){
-/*
-
-        ArrayList<SoftwareJob> prioList = new ArrayList<>(jobsList);
-        ArrayList<SoftwareJob> costList = new ArrayList<>(jobsList);
-        ArrayList<SoftwareJob> ageList = new ArrayList<>(jobsList);
-
-        double prioCoeff = 1;
-        double costCoeff = 1.75;
-        double ageCoeff = 1.5;
-
-        prioList.sort( Comparator.comparing(SoftwareJob::getPriority));
-
-        costList.sort( Comparator.comparing(SoftwareJob::getCost));
-
-        ageList.sort( Comparator.comparing(SoftwareJob::getDateCreated));
-
-        HashMap<Double,SoftwareJob> rankedJobs = new HashMap<>();
-        prioList.forEach(s -> {
-            double score = (prioList.indexOf(s)+1)*prioCoeff + (costList.indexOf(s)+1)*costCoeff + (ageList.indexOf(s)+1)*ageCoeff;
-            rankedJobs.put(score , s);
+        jobsList.forEach(soft -> {
+            localGossiper.gossip(localNode, localNode, gossipPID, soft);
         });
-
-        jobsList.clear();
-        while(! rankedJobs.isEmpty()){
-            Double key = Collections.min(rankedJobs.keySet());
-            jobsList.add(rankedJobs.remove(key));
-        }
     }
 
-    public int numberOfJobs(){
-        return jobsList.size();
-    */}
+    public abstract Scheduler clone();
+
+    // Implementing classes should implement their strategy in this class.
+    public abstract void updateTasks(Node node);
 
     public ArrayList<String> getJobList(){
 
