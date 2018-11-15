1272e8b7449907b893259866c786a4f9af8a5b38
Stefan Körner <stefankoerner@outlook.com>
2018-11-15 11:57:41 +0100

    Interval Graph (This time correctly)

--- plot/2d_graph.py
+++ plot/2d_graph.py
@@ -1,118 +1,67 @@
 import pandas
+import matplotlib.pyplot as plt
 import argparse
 import os
-from textwrap import dedent
-from texttable import Texttable
+import math
 
+interval = 10
 
-# Sets the interval for the data we want to look at (1-400):
-interval = 20
+def plotter(file_seq, path):
+    
+        coord = pandas.read_csv(path+"/graph_dump"+file_seq+".dat", header=None, delimiter=';')
+        neighbors = path+"/neighbors_dump"+file_seq+".dat"
+        print("Processing "+"/progress_dump"+file_seq+".dat")
+        progress = pandas.read_csv(path+"/progress_dump"+file_seq+".dat", delimiter=';')
 
+        #define subplot graph size
+        hauteur = math.floor(math.sqrt(len(progress.columns)))
+        largeur = math.ceil(math.sqrt(len(progress.columns)))
 
-f = open("log.txt", "w+")
+        key_index=1
+        for haut in  range(0,hauteur):
+                for large in range(1,largeur+1):
+                        if (key_index < len(progress.columns)-1):
+                                plt.subplot(hauteur, largeur, (largeur*haut)+(large), aspect='equal')
+                                plt.title(progress.columns[key_index])
+                                plt.axis('square')
 
+                                #plot neigbors relationships
+                                for line in open(neighbors):
+                                        neighbors_list = line.rstrip(';\n').split(';')
+                                        for node in range(1, len(neighbors_list)):
+                                                A = int(neighbors_list[0])
+                                                B = int(neighbors_list[node])
+                                                plt.plot([coord.iloc[A,1], coord.iloc[B,1]], [coord.iloc[A,2], coord.iloc[B,2]],  linewidth=0.2, zorder=-1, c='0.5')
 
-def progressSinceLastInterval(file_seq, path, id, packageNr):
+                                # Plot nodes
+                                plt.scatter(coord.iloc[:,1], coord.iloc[:,2], s=9, zorder=1, c=progress.iloc[:,key_index], vmin=0, vmax=100)
+                                plt.xticks([], [])
+                                plt.yticks([], [])
+                                plt.xlim(0,1000)
+                                plt.ylim(0,1000)
 
-    fileNumber = int(file_seq) - interval
-    fileNumerAsString = str(fileNumber).zfill(8)
-
-    if fileNumber > 0:
-        progressOld = pandas.read_csv(
-            path + "/progress_dump" + fileNumerAsString + ".dat", delimiter=';')
-
-        # print(progressOld)
-    else:
-        return
-
-
-    progressNew = pandas.read_csv(
-        path + "/progress_dump" + file_seq + ".dat", delimiter=';')
-
-    if interval>19:
-
-      progress = int(progressNew.iloc[id, packageNr]) - \
-      int(progressOld.iloc[id, packageNr])
-
-      return progress
-
-    else:
-      return
-
-
-def printTable(coord, progress, storage, file_seq, path):
-
-    print("--------------------------------------------------------")
-    print("Looking @ " + file_seq)
-    print("--------------------------------------------------------")
-
-    f.write("\n Looking @ " + file_seq + "\n")
-    t = Texttable()
-    t.add_rows(
-        [['Node ID', 'X', 'Y', 'Storage', 'Progress', '+%(1)', '+%(2)', 'Neighbors']])
-    A = 0
-
-    neighbors = path + "/neighbors_dump" + file_seq + ".dat"
-
-    for line in open(neighbors):
-
-        neighbors_list = line.rstrip(';\n').split(';')
-        t.add_row([coord.iloc[A, 0], coord.iloc[A, 1], coord.iloc[A, 2], storage.iloc[A, 1], progress.iloc[
-                  A, :], progressSinceLastInterval(file_seq, path, A, 1), progressSinceLastInterval(file_seq, path, A, 2), neighbors_list])
-        A += 1
-
-    print(t.draw())
-    f.write(t.draw())
-
-    t.reset()
-
-
-def getCoords(file_seq, path):
-
-    coord = pandas.read_csv(path + "/graph_dump" +
-                            file_seq + ".dat", header=None, delimiter=';')
-
-    # print(coord)
-
-    return coord
-
-
-def getProgress(file_seq, path):
-
-    progress = pandas.read_csv(
-        path + "/progress_dump" + file_seq + ".dat", delimiter=';')
-
-    # print(progress)
-
-    return progress
-
-
-def getStorage(file_seq, path):
-
-    storage = pandas.read_csv(
-        path + "/storage_dump" + file_seq + ".dat", delimiter=';')
-
-    # print(storage)
-
-    return(storage)
+                                key_index+=1
+        #plt.subplots_adjust() 
+        #plt.tight_layout()
+        #plt.savefig("figs/"+seq+'.pdf')
+        plt.savefig("figs/progress"+seq+'.png', dpi = (200))
+        plt.close()
 
 
 if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(
-        description='turn datafile into 2d map of nodes')
-    parser.add_argument('path', type=str, help='path to .dat Files')
+    
+    parser = argparse.ArgumentParser(description='turn datafile into 2d map of nodes')
+    parser.add_argument('path', type=str, help='path to .dat files')
     args = parser.parse_args()
-
-    Files = []
+    
+    # get list of files
+    files = []
     for dat in os.listdir(args.path):
         if dat.startswith("graph") and dat.endswith(".dat"):
-            Files.append(dat.lstrip("graph_dump").rstrip(".dat"))
+                files.append(dat.lstrip("graph_dump").rstrip(".dat"))               
 
-    for seq in sorted(Files):
-
-        if int(seq) % interval == 0:
-
-            printTable(getCoords(seq, args.path),  getProgress(
-                seq, args.path), getStorage(seq, args.path), seq, args.path)
-    f.close()
+    # plot each file
+    
+    for seq in sorted(files):
+        if int(seq)%interval== 0:
+          plotter(seq,args.path)
